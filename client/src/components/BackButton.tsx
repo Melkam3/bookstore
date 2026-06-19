d6d614ea83236226c780866d5d5ad1855e361f94
Melkam3 <melkter3@gmail.com>
2026-06-19 08:00:53 +0300

    creating api

--- client/src/components/BackButton.tsx
+++ client/src/components/BackButton.tsx
@@ -4,7 +4,7 @@ import { BsArrowLeft } from "react-icons/bs"
 function BackButton() {
   return (
     <div>
-     <BsArrowLeft/>
+     <BsArrowLeft  className="text-3xl bg-blue-500 m-4 p-2"/>
     </div>
   )
 }
--- client/src/pages/CreateBook.tsx
+++ client/src/pages/CreateBook.tsx
@@ -1,8 +1,44 @@
+import { useState } from "react"
+import BackButton from "../components/BackButton";
 
 
 function CreateBook() {
+  const [author, setAuthor] = useState("");
+  const [title, setTitle] = useState("")
+  const [publishYear, setPublishYear] = useState()
+
+
   return (
-    <div>CreateBook</div>
+    <div className=" ">
+        <BackButton/>
+      <form action="" className="flex flex-col gap-2 justify-center items-center mt-10 bg-blue-300 w-125 mx-auto ">
+        <h1>Create Book</h1>
+        <div className="">
+          <span>Title: </span>
+          <input
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+            className="border  "
+            type="text" />
+        </div>
+        <div>
+          <span>Title: </span>
+          <input
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+            className="border"
+            type="text" />
+        </div>
+        <div>
+          <span>Title: </span>
+          <input
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+            className="border"
+            type="text" />
+        </div>
+      </form>
+    </div>
   )
 }
 
